2194df561103f1258f5ec4e16778b396ce0764ff
Jose Motta <josemotta@trend.net.br>
2014-01-14 20:36:51 -0200

    [#63737510] add web paths to send IR commands

--- Home/Home/HomeStation/Program.cs
+++ Home/Home/HomeStation/Program.cs
@@ -23,6 +23,8 @@ namespace Home
         static Credential ServerCredential;//server security
         static Configuration ServerConfiguration;//configuration settings
         static DhtSensor Sensor;//humidity and temperature sensor
+        static InfraredTransmitter InfraredTransmitter;
+        static InfraredCodecNEC InfraredCodec;
 
         static Double temperature = 0;  // graus Celsius
         static Double humidity = 0;     // %
@@ -44,8 +46,8 @@ namespace Home
             NecProtocolDecoder.OnIRCommandReceived += NecDecoder_OnIRCommandReceived;
 
             //IRTX: Infrared Encoder
-            var irtx = new InfraredTransmitter(Pins.GPIO_PIN_D8);
-            var codec = new InfraredCodecNEC(irtx);
+            InfraredTransmitter = new InfraredTransmitter(Pins.GPIO_PIN_D8);
+            InfraredCodec = new InfraredCodecNEC(InfraredTransmitter);
 
             Thread.Sleep(1000);
 
@@ -83,7 +85,39 @@ namespace Home
 
         static void Server_OnRequestReceived(HttpRequest Request, HttpResponse Response)
         {
-            if (Request.RequestedFile != null)
+            int cmd = 0;
+
+            if (Request.RequestedCommand != null)
+            {
+                switch (Request.RequestedCommand.ToLower())
+                {
+                    case "on":
+                        cmd = 0x0D;         // ON
+                        break;
+                    case "off":
+                        cmd = 0x1F;         // OFF
+                        break;
+                    case "white":
+                        cmd = 0x15;         // WHITE
+                        break;
+                    case "green":
+                        cmd = 0x1B;         // GREEN
+                        break;
+                    case "red":
+                        cmd = 0x19;         // RED
+                        break;
+                    case "blue":
+                        cmd = 0x11;         // BLUE
+                        break;
+                    default:                // NONE
+                        break;
+                }
+
+                if (cmd != 0) InfraredCodec.Send(0xFF, cmd);    // Address is ignored by current led stripes
+
+                Response.WriteFilesList("Comando " + Request.RequestedCommand.ToLower() + ": Addr=0xFF Cmd=" + IntToHexString(cmd));
+            }
+            else if (Request.RequestedFile != null)
             {
                 string FullFileName = Request.FilesPath + Request.RequestedFile;
                 if (File.Exists(FullFileName))
@@ -122,6 +156,35 @@ namespace Home
             for (int i = 0; i < 8; i++)
             {
                 ulong x = val & (ulong)0x0000000F;
+                val = val >> 4;
+                switch (x)
+                {
+                    case 0: s = '0' + s; break;
+                    case 1: s = '1' + s; break;
+                    case 2: s = '2' + s; break;
+                    case 3: s = '3' + s; break;
+                    case 4: s = '4' + s; break;
+                    case 5: s = '5' + s; break;
+                    case 6: s = '6' + s; break;
+                    case 7: s = '7' + s; break;
+                    case 8: s = '8' + s; break;
+                    case 9: s = '9' + s; break;
+                    case 10: s = 'A' + s; break;
+                    case 11: s = 'B' + s; break;
+                    case 12: s = 'C' + s; break;
+                    case 13: s = 'D' + s; break;
+                    case 14: s = 'E' + s; break;
+                    case 15: s = 'F' + s; break;
+                }
+            }
+            return s;
+        }
+        public static string IntToHexString(int val)
+        {
+            string s = "";
+            for (int i = 0; i < 2; i++)
+            {
+                int x = val & 0x0F;
                 val = val >> 4;
                 switch (x)
                 {

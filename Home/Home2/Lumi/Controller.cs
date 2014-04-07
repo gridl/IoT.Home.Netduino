4ce8fb845ef71a3ce79cff9a04e51112f863c740
Jose Motta <josemotta@trend.net.br>
2014-04-06 22:18:37 -0300

    [#68953824] add Lumi & Light library

---
+++ Home/Home2/Lumi/Controller.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace VLumi
+{
+    public class Controller
+    {
+        public Controller()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Home/Home2/Lumi/LumiCmd.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace VLumi
+{
+    public enum LumiCmd
+    {
+        /// <summary>
+        /// Turn Virtual Luminary On
+        /// </summary>
+        On = 1,
+        /// <summary>
+        /// Turn Virtual Luminary Off
+        /// </summary>
+        Off = 2,
+        /// <summary>
+        /// Red Light
+        /// </summary>
+        Red = 3,
+        /// <summary>
+        /// Green Light
+        /// </summary>
+        Green = 4,
+        /// <summary>
+        /// Blue Light
+        /// </summary>
+        Blue = 5,
+        /// <summary>
+        /// White Light
+        /// </summary>
+        White = 6,
+        /// <summary>
+        /// White Flash
+        /// </summary>
+        Flash = 90,
+    }
+}
